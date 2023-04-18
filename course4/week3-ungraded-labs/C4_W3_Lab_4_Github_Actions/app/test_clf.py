51d5b9583940496740c133c12575105ee0677fae
Abonia SOJASINGARAYAR <aboniaa@gmail.com>
2023-04-18 13:49:36 +0200

    test github action

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
@@ -8,7 +8,7 @@ def test_accuracy():
     with open("data/test_data.pkl", "rb") as file:
         test_data = pickle.load(file)
 
-    # Unpack the tuple a lets test3
+    # Unpack the tuple a lets testing
     X_test, y_test = test_data
 
     # Compute accuracy of classifier
